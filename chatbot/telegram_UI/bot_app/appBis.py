6182d4acbdc6c8436504f5b5521680378fccbf5f
Samuli Lehtinen <slehtinen2000@gmail.com>
2024-04-11 17:22:19 +0200

    ApiKey registered in reddis db OK + format {"userID" : {"apiKey" : "userAPIKEY", "messageHistory" : "[List of dict of exchange of msg]"}}

--- chatbot/telegram_UI/bot_app/appBis.py
+++ chatbot/telegram_UI/bot_app/appBis.py
@@ -20,8 +20,6 @@ import redis
 
 import requests
 
-
-
 # Initialisation du logger
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -89,6 +87,32 @@ async def write_to_redis(key, value):
     # Optionally, you can specify an expiration time for the key
     # r.expire(key, 3600)  # This would expire the key in 1 hour (3600 seconds)
 
+# Function to write a value to a key in a Redis database
+async def write_dict_to_redis(dict_key, dict_mapping):
+    # Connect to Redis
+    print("Connecting to redis / host : ", REDIS_NET, " port : ", REDIS_PORT)
+    r = redis.Redis(host=REDIS_NET, port=REDIS_PORT, db=0)
+    print("Writing to redis:", dict_key, dict_mapping)
+    # Set the value for the given key
+    r.hset(dict_key, mapping=dict_mapping)
+
+    # Optionally, you can specify an expiration time for the key
+    # r.expire(key, 3600)  # This would expire the key in 1 hour (3600 seconds)
+
+# Function to write a value to a key in a Redis database
+async def read_dict_from_redis(dict_key):
+    # Connect to Redis
+    print("Connecting to redis / host : ", REDIS_NET, " port : ", REDIS_PORT)
+    r = redis.Redis(host=REDIS_NET, port=REDIS_PORT, db=0)
+    
+    # Set the value for the given key
+    value = r.hgetall(dict_key)
+    print("reading from redis:", value)
+    return value if value else None
+ 
+    # Optionally, you can specify an expiration time for the key
+    # r.expire(key, 3600)  # This would expire the key in 1 hour (3600 seconds)
+
 
 @dp.message(StringFilter("/getHistory"))
 async def get_history(message: types.Message):
@@ -97,17 +121,18 @@ async def get_history(message: types.Message):
     """
     user_id = message.from_user.id
 
-    chat_history_key = f"{user_id}_chatHistory"
-    print("want history : ", chat_history_key)
+    userID_key = f"{user_id}"
+    print("want history from user : ", userID_key)
     # Read chat history from Redis
-    chat_history = await read_from_redis(chat_history_key)
-    print("chat history : ", chat_history)
-    if chat_history:
-        #chat_history_text = ""
-        #for message in chat_history:
-        #    chat_history_text += f"User: {message['user']}\nSystem: {message['system']}\n\n"
-        #print(chat_history_text)
-        # Send the chat history
+    user_data = await read_dict_from_redis(userID_key)
+    print("chat history : ", user_data)
+    if user_data:
+        # Convert bytes to dictionary
+        existing_userData_decoded = {key.decode(): value.decode() for key, value in user_data.items()}
+        
+        chat_history = str(existing_userData_decoded['messageHistory'])
+
+
         await message.reply(chat_history)
     else:
         await message.reply("No chat history found.")
@@ -121,7 +146,7 @@ async def start(message: types.Message):
     user_id = message.from_user.id
 
     if user_id not in memory:
-        memory[user_id] = {"userID": user_id, "messageHistory": []}
+        memory[user_id] = {"userID": user_id, "api_key" : "empty","messageHistory": [], "hasAPIKey": '0'}
         # username does not exist it prints "None" 
         # username = message.from_user.username
         # print(username)
@@ -129,15 +154,14 @@ async def start(message: types.Message):
         
         all_users_key = "allUsers"
         
-        chat_history_key = f"{user_id}_chatHistory"
-        user_api_key = f"{user_id}_apiKey"
+        userID_key = f"{user_id}"
 
+        empty_dict = {'api_key': 'empty', 'messageHistory': 'empty'}
         # Write username to Redis
         # await write_to_redis(username_key, username)
         # Write an empty list to Redis for the chat history
-
-        await write_to_redis(chat_history_key, "empty chat history")
-        await write_to_redis(user_api_key, "empty api key")
+        await write_dict_to_redis(userID_key, empty_dict)
+        #await write_to_redis(chat_history_key, "empty chat history")
 
     # print(memory)
     await message.reply("Session started. You can start chatting.")
@@ -153,16 +177,29 @@ async def start(message: types.Message):
     Handler for the /setApiKey command to register a userApiKey.
     """
     user_id = message.from_user.id  
-    splitted_message = message.text.split(maxsplit=1)
-    if len(splitted_message) < 2:
-        await message.reply("Please provide an API key. \n Usage: /setApiKey <API_KEY>")
+    
+    if user_id in memory and memory[user_id]["hasAPIKey"] == '0':
+        await message.reply("Your next message will be considered as your API key.")
+        memory[user_id]["hasAPIKey"] = '1'
+        
+        # consider that the next message will be the API key so now user has API key
+        user_data = await read_dict_from_redis(f"{user_id}")
+        print("user_data:", user_data)
+        if user_data:
+            # Convert bytes to dictionary
+            existing_userData_decoded = {key.decode(): value.decode() for key, value in user_data.items()}
+            print("existing_userData_decoded:", existing_userData_decoded)
+            # Append message_dict to "messageHistory"
+            existing_userData_decoded['hasAPIKey'] = '1'
+            # Serialize updated data back to bytes
+            updated_userData = {"api_key" : existing_userData_decoded["api_key"], "messageHistory" : existing_userData_decoded["messageHistory"]}
+        await write_dict_to_redis(f"{user_id}", updated_userData)
         return
-    elif user_id in memory:
-        user_api_key = f"{user_id}_apiKey"
-        await write_to_redis(user_api_key, splitted_message[1])
-        await message.reply("API key set successfully.")
+    
+    elif user_id in memory and memory[user_id]["hasAPIKey"] == '1' or memory[user_id]["hasAPIKey"] == 'S':
+        await message.reply("your api key has already been set.")
         return
-    else : 
+    elif user_id not in memory: 
         await message.reply("Please start a session first.")
         return
 
@@ -171,9 +208,8 @@ async def start(message: types.Message):
 
 async def send_data_to_server(data, user_id):
     try:
-        chat_history_key = f"{user_id}_chatHistory"
+        userID_key = f"{user_id}"
         # Construct the dictionary with user and system messages
-
 
         response = requests.post(f"{SERVER_URL}/messages", json=data)
         print("Response from server:",  response.json())
@@ -183,9 +219,22 @@ async def send_data_to_server(data, user_id):
         }
         # Append the dictionary to message history
         memory[user_id]["messageHistory"].append(message_dict)
+        redis_data = str(memory[user_id]["messageHistory"])
         print("Writing to redis:", memory[user_id]["messageHistory"])
 
-        await write_to_redis(chat_history_key, str(memory[user_id]["messageHistory"]))
+
+        existing_userData = await read_dict_from_redis(userID_key)
+        print("existing_userData:", existing_userData)
+        if existing_userData:
+            # Convert bytes to dictionary
+            existing_userData_decoded = {key.decode(): value.decode() for key, value in existing_userData.items()}
+            print("existing_userData_decoded:", existing_userData_decoded)
+            # Append message_dict to "messageHistory"
+            existing_userData_decoded['messageHistory'] = redis_data
+            # Serialize updated data back to bytes
+            updated_userData = {"api_key" : existing_userData_decoded["api_key"], "messageHistory" : redis_data}
+
+        await write_dict_to_redis(userID_key, updated_userData)
 
         return response.json()["response"]
     except Exception as e:
@@ -207,25 +256,45 @@ async def echo_handler(message: types.Message) -> None:
 
     print("entering try")
     try:
-
-        data_to_send = {
-            "userId": user_id,
-            # "messageHistory": memory[user_id]["messageHistory"]
-            "message" : message.text
-        }
-
-        print("Sending to srv")
-
-        # Send data to the server asynchronously
-        response = await send_data_to_server(data_to_send, user_id)  # Get the response
         
-        print("Received from server:", response)
-        if response is None:
-            default_message = "Sorry, there was an issue processing your request."
-            await message.answer(default_message)
-        else:   
-            print(memory)
-            await message.answer(response)  # Use the response in message.answer()
+        if memory[user_id]["hasAPIKey"] == '1':
+            # write in redis the api key and reply only that the api key has been set
+            user_data = await read_dict_from_redis(f"{user_id}")
+            print("user_data:", user_data)
+            if user_data:
+                # Convert bytes to dictionary
+                existing_userData_decoded = {key.decode(): value.decode() for key, value in user_data.items()}
+                print("existing_userData_decoded:", existing_userData_decoded)
+                # Append message_dict to "messageHistory"
+                existing_userData_decoded['api_key'] = message.text
+                # Serialize updated data back to bytes
+                updated_userData = {"api_key" : message.text, "messageHistory" : existing_userData_decoded["messageHistory"]}
+            await write_dict_to_redis(f"{user_id}", updated_userData)
+            await message.answer("Your API key has been set.")
+            memory[user_id]["hasAPIKey"] = 'S'
+            return
+        
+        elif memory[user_id]["hasAPIKey"] == 'S' or memory[user_id]["hasAPIKey"] == '0':
+
+
+            data_to_send = {
+                "userId": user_id,
+                # "messageHistory": memory[user_id]["messageHistory"]
+                "message" : message.text
+            }
+
+            print("Sending to srv")
+
+            # Send data to the server asynchronously
+            response = await send_data_to_server(data_to_send, user_id)  # Get the response
+            
+            print("Received from server:", response)
+            if response is None:
+                default_message = "Sorry, there was an issue processing your request."
+                await message.answer(default_message)
+            else:   
+                print(memory)
+                await message.answer(response)  # Use the response in message.answer()
 
     except TypeError:
         # But not all the types is supported to be copied so need to handle it
