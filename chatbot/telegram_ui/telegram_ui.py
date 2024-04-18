def37c23103dbdae2330eba85c1e3680d7b0aa36
ODAncona <olivier.dancona@heig-vd.ch>
2024-04-18 18:32:36 +0200

    COMMIT

--- chatbot/telegram_ui/telegram_ui.py
+++ chatbot/telegram_ui/telegram_ui.py
@@ -5,18 +5,20 @@ and retrieves information from connected APIs.
 
 # ~~~ Imports ~~~
 import asyncio
-import sys
 import os
+import sys
+from fastapi import FastAPI, HTTPException
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+
+import redis
+import httpx
+
 import logging
 from colorlog import ColoredFormatter
 
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters import CommandStart, Filter
-from aiogram.enums import ParseMode
-from aiogram.types import Message
+import uvicorn
 
-import redis
-import requests
 
 # ___________________________________________________________________________________________
 # TEMPORARY: Load environment variables from .env file
@@ -30,9 +32,8 @@ warnings.filterwarnings("ignore")
 # ___________________________________________________________________________________________
 
 
-# ~~~ Configuration du Logger ~~~
-def get_logger():
-    global logger
+# ~~~ Logger Configuration ~~~
+def setup_logging():
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
 
@@ -52,287 +53,148 @@ def get_logger():
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(formatter)
     logger.addHandler(console_handler)
-
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-
     return logger
 
 
-get_logger()
-
-# ~~~ Variables globales ~~~
-global dp
-global chatID
-global bot
-global TELEGRAM_BOT_TOKEN
-memory = {}
-redis_client = None
-
-WELCOME_MESSAGE = """Welcome to AIFred. :-)
-I am an autonomous API agent, capable to give you information from the APIs that I have in stock.
-You can start a session by sending /startSession."""
-
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-
-ORCHESTRATOR_HOST = os.getenv("ORCHESTRATOR_HOST")
-ORCHESTRATOR_PORT = os.getenv("ORCHESTRATOR_PORT")
-
-SERVER_URL = f"http://{ORCHESTRATOR_HOST}:{ORCHESTRATOR_PORT}"
-
-if not TELEGRAM_BOT_TOKEN:
-    logger.error("Please provide a valid Telegram Bot Token.")
-    sys.exit(1)
-
-bot = Bot(token=TELEGRAM_BOT_TOKEN, parse_mode=ParseMode.HTML)
-dp = Dispatcher()
+# ~~~ Init bot ~~~
+def init_bot():
+    TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+    if not TOKEN:
+        logger.error("TELEGRAM_BOT_TOKEN is not set")
+        sys.exit(1)
+    bot = Bot(token=TOKEN)
+    dp = Dispatcher()
+    return bot, dp
 
 
 # ~~~ Connection to Redis ~~~
-def connect_to_redis():
+def init_redis():
     REDIS_HOST = os.getenv(
         "REDIS_SERVER_HOST", "redisdb-master.default.svc.cluster.local"
     )
+    REDIS_HOST = "127.0.0.1"
     REDIS_PORT = os.getenv("REDIS_SERVER_HTTP_PORT", "6379")
     REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
+    REDIS_PASSWORD = "YnkNRT0jv5"
     logger.info(
         f"Connecting to redis / host : {REDIS_HOST} port : {REDIS_PORT}"
     )
     r = redis.Redis(
-        host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD, db=0
+        host=REDIS_HOST,
+        port=REDIS_PORT,
+        password=REDIS_PASSWORD,
+        db=0,
+        decode_responses=True,
     )
     return r
 
 
 # ~~~ Redis Functions ~~~
-async def write_userdata_to_redis(dict_key, dict_mapping):
-    logger.info(f"Writing to redis: {dict_key} {dict_mapping}")
-    redis_client.hset(dict_key, mapping=dict_mapping)
+def write_userdata_to_redis(redis_client, key, dict_mapping):
+    logger.info(f"Writing to redis: {key} {dict_mapping}")
+    redis_client.hset(key, mapping=dict_mapping)
 
 
-async def read_userdata_from_redis(dict_key):
-    value = redis_client.hgetall(dict_key)
+def read_userdata_from_redis(redis_client, key):
+    value = redis_client.hgetall(key)
     logger.info(f"reading from redis: {value}")
     return value if value else None
 
 
-# ~~~ String Filter ~~~
-class StringFilter(Filter):
-    def __init__(self, my_text: str) -> None:
-        self.my_text = my_text
+# ~~~ Variables globales ~~~
+logger = setup_logging()
+app = FastAPI()
+bot, dp = init_bot()
+redis_db = init_redis()
 
-    async def __call__(self, message: Message) -> bool:
-        return message.text == self.my_text
+WELCOME_MESSAGE = """Welcome to SwissAi Center :-)
+I am AIFred, an autonomous API agent, capable to give you information from the APIs that I have in stock.
+
+"""
 
 
-# ~~~ Command Handlers ~~~
-@dp.message(StringFilter("/getHistory"))
-async def get_history(message: types.Message):
+# ~~~ FastAPI listener ~~~
+@app.get("/status/")
+async def get_status():
     """
-    Handler for the /getHistory command to get the chat history.
+    Check the status of the API.
+
+    Returns
+    -------
+    dict
     """
-    user_id = message.from_user.id
-    logger.info(f"want history from user : {user_id}")
-
-    # Read chat history from Redis
-    user_data = await read_userdata_from_redis(user_id)
-    logger.info(f"chat history : {user_data}")
-
-    if user_data:
-        existing_userData_decoded = {
-            key.decode(): value.decode() for key, value in user_data.items()
-        }
-        chat_history = str(existing_userData_decoded["messageHistory"])
-        await message.reply(chat_history)
-    else:
-        await message.reply("No chat history found.")
+    return {"status": "API is running"}
 
 
-@dp.message(StringFilter("/startSession"))
-async def startSession(message: types.Message):
-    """
-    Handler for the /startSession command to start a session.
-    """
-    user_id = message.from_user.id
-
-    if user_id not in memory:
-        memory[user_id] = {
-            "userID": user_id,
-            "api_key": "empty",
-            "messageHistory": [],
-            "hasAPIKey": "0",
-        }
-
-        userID_key = f"{user_id}"
-
-        empty_dict = {"api_key": "empty", "messageHistory": "empty"}
-        await write_userdata_to_redis(userID_key, empty_dict)
-
-    await message.reply("Session started. You can start chatting.")
-
-
-@dp.message(CommandStart())
-async def start(message: types.Message):
-    await message.reply(WELCOME_MESSAGE)
-
-
-# TODO: implement a service to set the API key of telegram user externally
-# @dp.message(StringFilter("/setApiKey"))
-# async def start(message: types.Message):
-#     """
-#     Handler for the /setApiKey command to register a userApiKey.
-#     """
-#     user_id = message.from_user.id
-
-#     if user_id in memory and memory[user_id]["hasAPIKey"] == '0':
-#         await message.reply("Your next message will be considered as your API key.")
-#         memory[user_id]["hasAPIKey"] = '1'
-
-#         # consider that the next message will be the API key so now user has API key
-#         user_data = await read_dict_from_redis(f"{user_id}")
-#         logger.info(f"user_data: {user_data}")
-#         if user_data:
-#             # Convert bytes to dictionary
-#             existing_userData_decoded = {key.decode(): value.decode() for key, value in user_data.items()}
-#             logger.info(f"existing_userData_decoded: {existing_userData_decoded}")
-#             # Append message_dict to "messageHistory"
-#             existing_userData_decoded['hasAPIKey'] = '1'
-#             # Serialize updated data back to bytes
-#             updated_userData = {"api_key" : existing_userData_decoded["api_key"], "messageHistory" : existing_userData_decoded["messageHistory"]}
-#         await write_dict_to_redis(f"{user_id}", updated_userData)
-#         return
-
-#     elif user_id in memory and memory[user_id]["hasAPIKey"] == '1' or memory[user_id]["hasAPIKey"] == 'S':
-#         await message.reply("your api key has already been set.")
-#         return
-#     elif user_id not in memory:
-#         await message.reply("Please start a session first.")
-#         return
-
-
-async def send_data_to_server(data, user_id):
+@app.post("/reply_to_user/")
+async def reply_to_user(user_id: str, message: str):
     try:
-        userID_key = f"{user_id}"
-        # Construct the dictionary with user and system messages
-
-        response = requests.post(f"{SERVER_URL}/messages", json=data)
-        logger.info(f"Response from server: {response.json()}")
-        message_dict = {
-            "user": data["message"],
-            "system": response.json()["response"],  # Server response
-        }
-        # Append the dictionary to message history
-        memory[user_id]["messageHistory"].append(message_dict)
-        redis_data = str(memory[user_id]["messageHistory"])
-        logger.info(f"Writing to redis: {memory[user_id]['messageHistory']}")
-
-        existing_userData = await read_userdata_from_redis(userID_key)
-        logger.info(f"existing_userData: {existing_userData}")
-        if existing_userData:
-            # Convert bytes to dictionary
-            existing_userData_decoded = {
-                key.decode(): value.decode()
-                for key, value in existing_userData.items()
-            }
-            logger.info(
-                f"existing_userData_decoded: {existing_userData_decoded}"
-            )
-            # Append message_dict to "messageHistory"
-            existing_userData_decoded["messageHistory"] = redis_data
-            # Serialize updated data back to bytes
-            updated_userData = {
-                "api_key": existing_userData_decoded["api_key"],
-                "messageHistory": redis_data,
-            }
-
-        await write_userdata_to_redis(userID_key, updated_userData)
-
-        return response.json()["response"]
+        await bot.send_message(chat_id=user_id, text=message)
+        logger.info(f"Message sent successfully to user {user_id}")
+        return {"message": "Message sent successfully"}
     except Exception as e:
-        logger.error(f"Error send_data: {e}")
+        logger.error(f"Failed to send message to user {user_id}: {str(e)}")
+        raise HTTPException(status_code=500, detail=str(e)) from e
+
+
+# ~~~ Aiogram handlers ~~~
+@dp.message(CommandStart())
+async def start_handler(message: types.Message):
+    user_id = str(message.from_user.id)
+    user_name = str(message.from_user.username)
+
+    if not redis_db.exists(user_id):
+        await message.reply(
+            f"{WELCOME_MESSAGE}Welcome {user_name}, just in case your user ID is: {str(user_id)} Make sure your OPENAI API key is set before you start chatting with me."
+        )
+        empty_dict = {"api_key": "", "messageHistory": ""}
+        write_userdata_to_redis(redis_db, user_id, empty_dict)
+    else:
+        await message.reply(f"{WELCOME_MESSAGE}Welcome back {user_name}!")
 
 
 @dp.message()
-async def echo_handler(message: types.Message) -> None:
-    """
-    Handler will forward receive a message back to the sender
-
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
-    """
-    # logger.info(message)
-    user_id = message.from_user.id
-    if user_id not in memory:
-        await message.answer("Please start a session first.")
-        return
-
-    logger.info("entering try")
-    try:
-
-        if memory[user_id]["hasAPIKey"] == "1":
-            # write in redis the api key and reply only that the api key has been set
-            user_data = await read_userdata_from_redis(f"{user_id}")
-            logger.info(
-                f"user_data: {user_data}",
-            )
-            if user_data:
-                # Convert bytes to dictionary
-                existing_userData_decoded = {
-                    key.decode(): value.decode()
-                    for key, value in user_data.items()
-                }
-                logger.info(
-                    f"existing_userData_decoded: {existing_userData_decoded}"
-                )
-                # Append message_dict to "messageHistory"
-                existing_userData_decoded["api_key"] = message.text
-                # Serialize updated data back to bytes
-                updated_userData = {
-                    "api_key": message.text,
-                    "messageHistory": existing_userData_decoded[
-                        "messageHistory"
-                    ],
-                }
-            await write_userdata_to_redis(f"{user_id}", updated_userData)
-            await message.answer("Your API key has been set.")
-            memory[user_id]["hasAPIKey"] = "S"
+async def echo_handler(message: types.Message):
+    user_id = str(message.from_user.id)
+    HOST = "192.168.49.2"
+    PORT = 32290
+    system_http_url = f"http://{HOST}:{PORT}/get_similar_apis/"
+    user_data = {"user_id": user_id, "prompt": message.text, "k": 3}
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.post(system_http_url, json=user_data)
+            if response.status_code == 200:
+                logger.info(f"Data sent successfully: {response.json()}")
+            else:
+                logger.error(f"Failed to send data: {system_http_url}")
+                logger.error(f"Failed to send data: {response}")
+        except Exception as e:
+            logger.error(f"Failed to send data: {str(e)}")
+            await message.answer("Failed to process your message.")
             return
 
-        elif (
-            memory[user_id]["hasAPIKey"] == "S"
-            or memory[user_id]["hasAPIKey"] == "0"
-        ):
 
-            data_to_send = {
-                "userId": user_id,
-                # "messageHistory": memory[user_id]["messageHistory"]
-                "message": message.text,
-            }
-
-            logger.info("Sending to srv")
-
-            response = await send_data_to_server(
-                data_to_send, user_id
-            )  # Get the response
-
-            logger.info(f"Received from server: {response}")
-            if response is None:
-                default_message = (
-                    "Sorry, there was an issue processing your request."
-                )
-                await message.answer(default_message)
-            else:
-                logger.info(memory)
-                await message.answer(
-                    response
-                )  # Use the response in message.answer()
-
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
+# ~~~ Start Telegram Bot ~~~
+async def start_telegram_bot():
+    try:
+        await dp.start_polling(bot)
+    except Exception as e:
+        logger.error(f"Unexpected error: {e}")
 
 
-async def main() -> None:
-    await dp.start_polling(bot)
+# ~~~ Start FastApi ~~~
+async def start_fastapi():
+    config = uvicorn.Config(app=app, host="0.0.0.0", port=8080, loop="asyncio")
+    server = uvicorn.Server(config)
+    await server.serve()
+
+
+# ~~~ Main Function ~~~
+async def start_app():
+    bot_task = asyncio.create_task(start_telegram_bot())
+    api_task = asyncio.create_task(start_fastapi())
+    await asyncio.gather(bot_task, api_task)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(start_app())
