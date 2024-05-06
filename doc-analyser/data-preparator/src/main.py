7a06e782a22d83a4cc911f10e4bb29b811a66d65
Jean-François Pasche <jeanfrancois.pasche@ik.me>
2024-05-06 14:29:49 +0200

    remove s3 prefix in coco file names

--- doc-analyser/data-preparator/src/main.py
+++ doc-analyser/data-preparator/src/main.py
@@ -57,20 +57,34 @@ def main():
                 f.write(zip_response.content)
                 with zipfile.ZipFile('data/out.zip', 'r') as zip_ref:
                     zip_ref.extractall('data/')
+
+            with open('data/result.json', 'r') as f:
+                result = json.load(f)
+
+            for img in result['images']:
+                img['file_name'] = img['file_name'].replace('s3://mse-pi-lstudio-source/', '')
+            
+            with open('data/coco_annotations.json', 'w') as f:
+                json.dump(result, f)
+                
+            os.remove('data/out.zip')
+            os.remove('data/result.json')
+            os.rmdir('data/images')
+
         except Exception as e:
             logging.error(e)
             return 1
         
-        os.remove('data/out.zip')
-        os.rmdir('data/images')
-        os.rename('data/result.json', 'data/coco_annotations.json')
     else:
         logging.info("COCO file already exists")
     
     with open('data/coco_annotations.json', 'r') as f:
         coco_annotations = json.load(f)
 
-    s3_files_endpoints = [ann['file_name'].replace('s3://mse-pi-lstudio-source/', '') for ann in coco_annotations['images'] if ann['file_name'].startswith('s3://')]
+    for img in coco_annotations['images']:
+        img['file_name'] = img['file_name'].replace('s3://mse-pi-lstudio-source/', '')
+
+    s3_files_endpoints = [ann['file_name'] for ann in coco_annotations['images']]
     s3_files_endpoints_set = set(s3_files_endpoints)
 
     logging.info(f"Found {len(s3_files_endpoints_set)} files in COCO annotations")
