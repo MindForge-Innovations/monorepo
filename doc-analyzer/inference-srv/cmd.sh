13941ffbd150d90a7c68850ae58d36eb20dc930c
Jean-François Pasche <jeanfrancois.pasche@ik.me>
2024-05-28 23:06:29 +0200

    failed attempt to use torchserve

---
+++ doc-analyzer/inference-srv/cmd.sh
@@ -0,0 +1,14 @@
+curl -X POST "http://doc-classifier.kube.isc.heia-fr.ch/predict/" -F "file=@data/Grotius_Dg-012.png"
+
+curl -X POST "http://localhost:8080/predictions/document_classifier" -T data/Grotius_Dg-012.png
+
+
+torch-model-archiver --model-name doc-classifier --version 0.1 --serialized-file src/model.pt
+
+torch-model-archiver --model-name document_classifier \
+                     --version 1.0 \
+                     --model-file src/model.pt \
+                     --serialized-file src/model.pt \
+                     --handler image_classifier \
+                     --export-path . \
+                     --extra-files src/config/model-config.json
